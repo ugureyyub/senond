4a4525a73e1849879bf27edba1874db544866d65
ƏYYUBOV İSMAYİL <115778685+ugureyyub@users.noreply.github.com>
2022-12-22 01:01:39 +0400

    Add files via upload
    bu bir testdir

---
+++ TEST.py
@@ -0,0 +1,23 @@
+meyveler = {
+            'elinin_meyveleri':{
+                                'alma': {
+                                        'qirmizi':5,
+                                        'yasil':10,
+                                          },
+            'armud': 4,
+            'erik': 8,
+            },
+
+          'velinin_meyveleri':{
+            'alma': 7,
+            'armud':9,
+            'banan':12,
+           },
+          'pirvelinin_meyveleri':{
+              'qoz':9,
+              'heyva':14,
+              'findiq':18,
+          }
+}
+
+print(meyveler['elinin_meyveleri']['alma'])
